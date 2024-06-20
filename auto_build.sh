0075e3b4eb49137b599f4b6b0aedc620508ea2e5
adan-shaw <adan_shaw@qq.com>
2024-06-20 16:09:48 +0800

    update

--- auto_build.sh
+++ auto_build.sh
@@ -1,14 +1,27 @@
 #!/bin/sh
 
 # 声明: 本auto_build.sh only for debian 10
+apt-get install libhiredis-dev 
+apt-get install libmariadb-dev libmariadb++-dev
+apt-get install openssl libssl-dev 
 
 
 
 # 先build 依赖
-cd ./Depend
-cd lua
+cd ./Depend/lua
 make -j4
 cd ../..
+
+# 补全依赖
+cd ./Depend
+ln -s /usr/include/mysql
+
+# 未改名
+ln -s /usr/include/hiredis 
+ln -s /usr/include/openssl 
+cd ..
+
+
 
 # 再build project
 mkdir build
@@ -17,6 +30,6 @@ cmake ..
 make -j4
 cd ..
 
-# 补全依赖
-cd ./Lib/Linux
-ln -s ../../Depend/lua/src/liblua.a
+
+
+# 这个project 还是半成品, 很多东西未完善!! (不可靠, 慎用)
