ef6f8824b5f9dbbf7b90347c661f6686058a7569
adan-shaw <adan_shaw@qq.com>
2024-06-25 19:37:31 +0800

    update

--- auto_build4debian1x.sh
+++ auto_build4debian1x.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 
 # 声明: 本auto_build.sh only for debian 10
+
+
+
+# 安装依赖
 apt-get install libhiredis-dev 
 apt-get install libmariadb-dev libmariadb++-dev
 apt-get install openssl libssl-dev 
@@ -12,11 +16,9 @@ cd ./Depend/lua
 make -j4
 cd ../..
 
-# 补全依赖
+# 补全依赖的头文件(需要根据linux 系统安装到位才行)
 cd ./Depend
 ln -s /usr/include/mysql
-
-# 未改名
 ln -s /usr/include/hiredis 
 ln -s /usr/include/openssl 
 cd ..
@@ -39,3 +41,10 @@ ln -s ../../Depend/lua/src/liblua.a
 
 
 # 这个project 还是半成品, 很多东西未完善!! (不可靠, 慎用)
+# 依赖list
+:<<!
+* mysql
+* redis
+* openssl
+* lua5.4
+!
